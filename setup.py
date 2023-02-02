49edf1cc4821f32bc3b8a6f7d326bb042313b7bd
nit0511 <nit51196@gmail.com>
2023-02-02 22:09:47 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="nitesh",
+    author_email="nit051196@gmail.com",
+    packages= find_packages(),
+    install_requires = get_requirements(),
+    )
